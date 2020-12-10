9783b9d970fbc39b6025ca75c53fa05584c38dbb
felipelopezortega <flopezortega@ucol.mx>
2020-12-10 15:49:01 -0600

    factorial function added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+
+        let fact = 1
+
+        for(let i=2 ; i<=numero; i = i + 1){
+
+            fact = fact * i
+        }
+
+        return fact
+    }
+
+
+
+
+}
 
 let app = new App();
+
+console.log(app.factorial(5))
