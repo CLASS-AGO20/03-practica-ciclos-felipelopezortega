9d53caa9477ed150b01a92b0e692f642f744c2c6
felipelopezortega <flopezortega@ucol.mx>
2020-12-10 15:58:21 -0600

    divisible function added

--- src/main.js
+++ src/main.js
@@ -12,11 +12,44 @@ export default class App {
         return fact
     }
 
+    /*convertirAString(numero){
 
+        let i=1
+        
+        while()
+
+    }*/
+
+    obtenerDivisibles(numero){
+
+        let i = 1
+        let divi = 0
+
+        do{
+
+            if(numero%i==0){
+
+                divi ++
+            }else{
+
+            }
+
+            i++
+
+        }while(i<=numero)
+
+        return divi
+    }
 
 
 }
 
 let app = new App();
 
+//Función factorial
 console.log(app.factorial(5))
+
+//Función asteriscos
+
+//Función divisible
+console.log(app.obtenerDivisibles(6))
